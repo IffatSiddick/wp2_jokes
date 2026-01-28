4104318780d1678d48a896396645899f19aecb93
IffatSiddick <saraiffat506@gmail.com>
2026-01-28 16:39:12 +0000

    all files redirect to index when needed

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -7,9 +7,9 @@ foreach($jokes as $joke): ?>
         (by <a href="mailto:<?=htmlspecialchars($joke['email'], ENT_QUOTES, 'UTF-8' );?>">
         <?=htmlspecialchars($joke['name'], ENT_QUOTES, 'UTF-8'); ?></a> on <?=$joke['jokedate'];?>)
 
-        <a href="editjoke.php?action=edit&id=<?=$joke['id']?>">Edit</a>
+        <a href="index.php?action=edit&id=<?=$joke['id']?>">Edit</a>
 
-        <form action="deletejoke.php?action=delete" method="post">
+        <form action="index.php?action=delete" method="post">
                 <input type="hidden" name="id" value="<?=$joke['id']?>">
                 <input type="submit" value="Delete">
         </form>
